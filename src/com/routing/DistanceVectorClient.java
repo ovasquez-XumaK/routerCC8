d962fc8f22113d81fd9cd57de6a3e109e12b3058
Jose <vasquezenriquez@gmail.com>
2014-11-09 00:23:03 -0600

    almost finish client server (how to know if table is changed?)

--- src/com/routing/DistanceVectorClient.java
+++ src/com/routing/DistanceVectorClient.java
@@ -1,6 +1,9 @@
 package com.routing;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -9,15 +12,21 @@ import java.util.Iterator;
 /**
  * Created by Jose on 05/11/2014.
  */
-public class DistanceVectorClient implements Runnable {
+public class DistanceVectorClient extends Thread {
 
     private AdjacentNode adjacentReference;
     public Socket adjacentSocket;
     public TableStructure tableOfStructure;
+    private static String valueFrom = "From";
+    private static String valueType = "Type";
+    private boolean converStarted = false;
+    private boolean isFirstTime = true;
+    private String valueName;
 
     public DistanceVectorClient(AdjacentNode adjacent, TableStructure table){
         adjacentReference = adjacent;
         tableOfStructure = table;
+
     }
 
     @Override
@@ -32,6 +41,22 @@ public class DistanceVectorClient implements Runnable {
                     e.printStackTrace();
                 }
             } else {
+                try{
+                    PrintWriter outputStream = new PrintWriter(this.adjacentSocket.getOutputStream(),true);
+                    if(!converStarted){
+                        BufferedReader inputSream = new BufferedReader(new InputStreamReader(this.adjacentSocket.getInputStream()));
+                        startCommunication(inputSream,outputStream);
+                    } else if(isFirstTime) {
+                        sendDVMessage(outputStream,false);
+                    } else {
+                        sendDVMessage(outputStream,true);
+                    }
+                    Thread.sleep( 30000 );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 //Agregar validaciones para enviar DV o KeepAlive
             }
         }
@@ -46,7 +71,7 @@ public class DistanceVectorClient implements Runnable {
             String peerNameTemp = keys.next().toString();
             ReachNode nodeTemp = tableOfStructure.getRoute(peerNameTemp);
             bodyMessage = bodyMessage + peerNameTemp + ":" + nodeTemp.getCost() + "\n";
-            numberOfLines = 0;
+            numberOfLines += 1;
         }
         message = "From:" + tableOfStructure.routerName + "\n";
         message = message + "Type:DV" + "\n";
@@ -60,5 +85,42 @@ public class DistanceVectorClient implements Runnable {
         String message = "From:" + tableOfStructure.routerName + "\n";
         message = message + "Type:KeepAlive" + "\n";
         return message;
+    }
+
+    public void startCommunication(BufferedReader inputSream,PrintWriter outputStream){
+        try {
+            String message = valueFrom + ":" + tableOfStructure.routerName + "\n";
+            message = message + valueType + ":HELLO";
+            outputStream.println(message);
+            //read response from adjacent
+            String inputMessage = inputSream.readLine();
+            String[] tokensInputMessage = inputMessage.split(":");
+            if (tokensInputMessage[0].equals(valueFrom)) {
+                valueName = tokensInputMessage[1];
+            }
+            if (valueName.equals(adjacentReference.getName())) {
+                tokensInputMessage = inputSream.readLine().split(":");
+                if (tokensInputMessage[0].equals(valueType)) {
+                    if (tokensInputMessage[1].equals("WELCOME")) {
+                        converStarted = true;
+                    }
+                }
+
+            } else {
+                System.out.println("Authentication Failure expected:" + adjacentReference.getName() + " Found:" + valueName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendDVMessage(PrintWriter outputStream,boolean isKeepAlive){
+        if(isKeepAlive){
+            String messageKeepAlive = buildKeepAlive();
+            outputStream.print(messageKeepAlive);
+        } else {
+            String messageToSend = buildDVMessage();
+            outputStream.print(messageToSend);
+        }
     }
 }
